b3e8d6f7c17ee1b145f2f681789d973841058bdc
Alexander Sivura <asivura@gmail.com>
2016-09-10 22:57:50 -0700

    MotionDetector added

---
+++ RacoonnRecognizer/RacoonnRecognizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RacoonnRecognizer
+//
+//  Created by Alexander Sivura on 9/10/16.
+//  Copyright © 2016 sivura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
