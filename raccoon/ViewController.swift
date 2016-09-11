89f6c1d4a31d6505107cb3803dbc9bdcce61170b
trierra <trierra.dev@gmail.com>
2016-09-10 22:32:27 -0700

    initial watson

--- raccoon/ViewController.swift
+++ raccoon/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func run(sender: AnyObject) {
+        print("runs")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
